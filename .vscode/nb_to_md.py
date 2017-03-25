db9474d5eda526c2584d7b6a07b31c11b569ad5c
yuki <123syokutaku@gmail.com>
2017-03-25 12:52:38 +0900

    スクリプトを画像に対応させた

--- .vscode/nb_to_md.py
+++ .vscode/nb_to_md.py
@@ -1,10 +1,10 @@
 import sys
 import re
 import os
+import shutil
+import urllib.parse
 
 nb_filename = sys.argv[1]
-target = r"^\$\$"
-repl = r"\$$\displaystyle "
 
 
 # ノートブックをマークダウンに変換
@@ -14,17 +14,39 @@ os.system("python -m nbconvert --to markdown --output-dir _posts " + nb_filename
 # 行頭にある"$$"を"\$$\displaystyle "に変更する
 # 本当に中央寄せを使いたいときは" $$"などにすればOK
 str = ""
+preserving_images = []
 md_filename = "_posts/" + os.path.split(nb_filename)[-1][:-5] + "md"
 with open(md_filename, encoding="utf-8") as f:
     for line in f.readlines():
-        if re.match(target, line):
-            str += re.sub(target, repl, line)
-        else:
-            str += line
+        line = re.sub(r"^\$\$", r"\$$\displaystyle ", line)
+        line = re.sub(r"\|", r"\|", line)
+        if ".png" in line:
+            # 画像があったらURLを差し替える
+            preserving_images.append(urllib.parse.unquote(line))
+            line = urllib.parse.unquote(line)
+            print(line)
+            if not ("(" in line and ")" in line):
+                continue
+            left = line.index("(")
+            line = line[:left + 1] + "/images/" + line[left + 1:]
+            print(line)
+        str += line
 
 # 書き込む
 with open(md_filename, "w", encoding="utf-8") as f:
     f.write(str)
 
-# nbconvertで作られる画像ファイルを削除(いらない)
-os.system("rm -r _posts/*_files")
+# nbconvertで作られる画像ファイルをimagesフォルダへ移動
+# 使っていないファイルは消す
+dir_name = os.path.split(nb_filename)[-1][:-6] + "_files"
+shutil.rmtree(os.path.join("images", dir_name), ignore_errors=True)
+shutil.move(os.path.join("_posts", dir_name), "images")
+for f in os.listdir(os.path.join("images", dir_name)):
+    # 線形探索
+    for line in preserving_images:
+        if f in line:
+            print("preserving", f)
+            break
+    else:
+        print("removing", f)
+        os.remove(os.path.join("images", dir_name, f))
