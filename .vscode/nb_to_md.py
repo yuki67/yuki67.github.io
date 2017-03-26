e3235430e67a32b7646994b5667d2f327136b4ae
yuki <123syokutaku@gmail.com>
2017-03-26 21:31:06 +0900

    md生成スクリプトにpandoc/はてなブログ用の設定を追加

--- .vscode/nb_to_md.py
+++ .vscode/nb_to_md.py
@@ -13,12 +13,14 @@ os.system("python -m nbconvert --to markdown --output-dir _posts " + nb_filename
 # デフォルトの状態だと数式が$$...$$となっていて中央寄せになるので
 # 行頭にある"$$"を"\$$\displaystyle "に変更する
 # 本当に中央寄せを使いたいときは" $$"などにすればOK
+# コメントアウトしてあるの2箇所はpandoc/はてなブログ用の設定
 str = ""
 preserving_images = []
 md_filename = "_posts/" + os.path.split(nb_filename)[-1][:-5] + "md"
 with open(md_filename, encoding="utf-8") as f:
     for line in f.readlines():
         line = re.sub(r"^\$\$", r"\$$\displaystyle ", line)
+        # line = line.replace("$$", r"$\displaystyle ", 1).replace("$$", "$", 1)
         line = re.sub(r"\|", r"\middle| ", line)
         if ".png" in line:
             # 画像があったらURLを差し替える
@@ -29,6 +31,7 @@ with open(md_filename, encoding="utf-8") as f:
                 continue
             left = line.index("(")
             line = line[:left + 1] + "/images/" + line[left + 1:]
+            # line = line[:left + 1] + "https://github.com/yuki67/yuki67.github.io/blob/master/images/" + line[left + 1:-2] + "?raw=true" + ")\n"
             print(line)
         str += line
 
